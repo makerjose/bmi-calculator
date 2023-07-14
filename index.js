96d020cbb902b4de27696e45c14ae1f48e5a45da
makerjose <josemakerdeng@gmail.com>
2023-07-14 09:14:05 +0300

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const urlEncodedParser = bodyParser.urlencoded({ extended: false });
@@ -104,7 +104,7 @@ function calculateAverageBMI(bmiValues) {
   
 
 app.listen(port, () => {
-  console.log('Server is listening on port 3000');
+  console.log('Server is listening on port ${port}');
 });
 
 
